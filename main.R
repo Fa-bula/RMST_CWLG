ce8b0f4243650f5f66b441c6816d9301d7698a26
fa_bula <fa_bula@mail.ru>
2023-08-24 23:41:13 +0300

    Added maxCombo

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 library("survival")
 library("survminer")
 library("plotly")
+library("nph")
 
 # Number of observations in each treatment groups
 n <- 100
@@ -12,9 +13,14 @@ N <- 100
 scale_1 <- 1
 # Scale parameter of the second (treatment) group
 scale_2 <- 1.6
+# List of shape parameters of Weibull distribution
+shape_list <- seq(1,2,0.1)
+# List of censoring probabilities
+p_cens_list <- seq(0.2,0.4,0.05)
 
-calculate_logrank_power <- function(params) {
-  H0_rejected = c()
+calculate_power <- function(params) {
+  H0_rejected_logrank = c()
+  H0_rejected_maxcombo = c()
   for (exp in (1:N)) {
     i <- 0
     df <- data.frame()
@@ -34,16 +40,30 @@ calculate_logrank_power <- function(params) {
         rbind(df, data.frame(usubjid, cohort, aval, event, scale, shape))
     }
     
-    survdiff <- survdiff(Surv(time = aval, event = event, type = 'right') ~ cohort, data = df)
+    logrank_pval <- survdiff(Surv(time = aval,
+                                  event = event,
+                                  type = 'right') ~ cohort,
+                             data = df)$pvalue
+
+    maxcombo_pval <- logrank.maxtest(df$aval,
+                                     df$event,
+                                     df$cohort,
+                                     "two.sided")$pmult
     
-    H0_rejected <- append(H0_rejected, survdiff$pvalue < alpha)
+    H0_rejected_logrank <-
+      append(H0_rejected_logrank, logrank_pval < alpha)
+    
+    H0_rejected_maxcombo <- append(H0_rejected_maxcombo,
+                                   maxcombo_pval < alpha)
   }
   
-  return (sum(H0_rejected) / length(H0_rejected))
+  newList <-
+    list(
+      "logrank_power" = (sum(H0_rejected_logrank) / length(H0_rejected_logrank)),
+      "maxcombo_power" = (sum(H0_rejected_maxcombo) / length(H0_rejected_maxcombo))
+    )
+  return(newList)
 }
-
-shape_list <- seq(1,2,0.1)
-p_cens_list <- seq(0.2,0.4,0.05)
 
 results <- data.frame()
 for (shape in shape_list) {
@@ -52,18 +72,35 @@ for (shape in shape_list) {
     params <- list()
     params[[1]] <- c(1, scale_1, p_cens)
     params[[2]] <- c(shape, scale_2, p_cens)
-    power <- calculate_logrank_power(params=params)
-    results <- rbind(results, data.frame(shape, p_cens, power))
+    powers <- calculate_power(params=params)
+    logrank_power <- powers$logrank_power
+    maxcombo_power <- powers$maxcombo_power
+    results <-
+      rbind(results,
+            data.frame(shape, p_cens, logrank_power, maxcombo_power))
   }
 }
 
 # 3D plot of results
-fig <- plot_ly(results, x = ~shape, y = ~p_cens, z = ~power, color = ~p_cens)
+fig <- plot_ly(results, x = ~shape, y = ~p_cens, z = ~logrank_power, color = ~p_cens)
 
-
-
-fig <- fig %>% add_mesh(x = c(0, 0, 2, 2), y = c(0, 1, 0, 1),
-                        z = c(0.8, 0.8, 0.8, 0.8), opacity=0.4)
+fig <-
+  fig %>% add_mesh(
+    x = c(
+      min(shape_list),
+      min(shape_list),
+      max(shape_list),
+      max(shape_list)
+    ),
+    y = c(
+      min(p_cens_list),
+      max(p_cens_list),
+      min(p_cens_list),
+      max(p_cens_list)
+    ),
+    z = c(0.8, 0.8, 0.8, 0.8),
+    opacity = 0.4
+  )
 
 fig <- fig %>% add_markers()
 
@@ -75,11 +112,50 @@ fig <- fig %>% layout(scene = list(xaxis = list(title = 'Shape'),
 
 fig
 
+fig <- plot_ly(results, x = ~shape, y = ~p_cens, z = ~maxcombo_power, color = ~p_cens)
+
+fig <-
+  fig %>% add_mesh(
+    x = c(
+      min(shape_list),
+      min(shape_list),
+      max(shape_list),
+      max(shape_list)
+    ),
+    y = c(
+      min(p_cens_list),
+      max(p_cens_list),
+      min(p_cens_list),
+      max(p_cens_list)
+    ),
+    z = c(0.8, 0.8, 0.8, 0.8),
+    opacity = 0.4
+  )
+
+fig <- fig %>% add_markers()
+
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'Shape'),
+                                   
+                                   yaxis = list(title = 'p_cens'),
+                                   
+                                   zaxis = list(title = 'MaxCombo test power')))
+
+fig
+
 # 2D plots of results
-results %>% ggplot(aes(x = shape, y = power)) + geom_point(aes(colour = p_cens)) +
+results %>%
+  ggplot(aes(x = shape, y = logrank_power)) +
+  geom_point(aes(colour = p_cens), size = 2) +
   geom_hline(yintercept = 0.8) +
   xlab('Shape') + ylab('Log-rank test power') +
   ggtitle(label = 'Log-rank test power')
+
+results %>%
+  ggplot(aes(x = shape, y = maxcombo_power)) +
+  geom_point(aes(colour = p_cens), size = 2) +
+  geom_hline(yintercept = 0.8) +
+  xlab('Shape') + ylab('Maxcombo log-rank test power') +
+  ggtitle(label = 'MaxCombo test power')
 
 # 2D plots of survival functions
 base <-
