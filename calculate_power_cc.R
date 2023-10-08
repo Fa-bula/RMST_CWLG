08c211f5748699209ed7d9d3700052a519341f63
Fa-bula <fa_bula@mail.ru>
2023-10-08 17:57:22 +0200

    Refactored code for results plot creation

--- calculate_power_cc.R
+++ calculate_power_cc.R
@@ -1,6 +1,7 @@
 calculate_power_cc <- function(params) {
   H0_rejected_logrank = c()
   H0_rejected_maxcombo = c()
+  H0_rejected_rmst = c()
   for (exp in (1:N)) {
     i <- 0
     df <- data.frame()
@@ -14,10 +15,10 @@ calculate_power_cc <- function(params) {
       aval <- rweibull(n = n, shape = shape, scale = scale)
       event <- rbinom(n, 1, 1 - p_cens)
       cohort <- sprintf("WEIBULL-%.2f-%.2f", shape, scale)
-      
+      arm <- i - 1
       usubjid <- sprintf("SUBJ-%03d", begin:end)
       df <-
-        rbind(df, data.frame(usubjid, cohort, aval, event, scale, shape))
+        rbind(df, data.frame(usubjid, cohort, arm, aval, event, scale, shape))
     }
     
     diff <- survdiff(Surv(time = aval,
@@ -32,17 +33,24 @@ calculate_power_cc <- function(params) {
                                      df$cohort,
                                      "two.sided")$pmult
     
+    rmst_pval <- rmst2(time=df$aval, status=df$event, alpha=alpha,
+                       arm=df$arm)[5]$unadjusted.result[1,4]
+    
     H0_rejected_logrank <-
       append(H0_rejected_logrank, logrank_pval < alpha)
     
     H0_rejected_maxcombo <- append(H0_rejected_maxcombo,
                                    maxcombo_pval < alpha)
+    
+    H0_rejected_rmst <- append(H0_rejected_rmst,
+                               rmst_pval < alpha)
   }
   
   newList <-
     list(
       "logrank_power" = (sum(H0_rejected_logrank) / length(H0_rejected_logrank)),
-      "maxcombo_power" = (sum(H0_rejected_maxcombo) / length(H0_rejected_maxcombo))
+      "maxcombo_power" = (sum(H0_rejected_maxcombo) / length(H0_rejected_maxcombo)),
+      "rmst_power" = (sum(H0_rejected_rmst) / length(H0_rejected_rmst))
     )
   return(newList)
 }
