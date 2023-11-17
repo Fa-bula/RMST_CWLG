b99062b7f87abdf2e703b020095a33fa8b3da7f1
Fa-bula <fa_bula@mail.ru>
2023-11-17 22:05:11 +0100

    Added plots by population size and trial duration

--- create_figures.R
+++ create_figures.R
@@ -1,62 +1,88 @@
 library("grid")
 library("gridExtra")
 library("plotly")
+library("eha")
+
+args <- list()
+for (n in n_list) {
+  for (t_duration in t_duration_list) {
+    args [[length(args) + 1]] <- list(
+      var = "logrank_power",
+      title = "Log-rank test",
+      n = n,
+      t_duration = t_duration
+    )
+    args [[length(args) + 1]] <- list(
+      var = "maxcombo_power",
+      title = "Maxcombo test",
+      n = n,
+      t_duration = t_duration
+    )
+    args [[length(args) + 1]] <- list(
+      var = "rmst_power",
+      title = "RMST test",
+      n = n,
+      t_duration = t_duration
+    )
+  }
+}
 
 # 2D plot of results
-cc_plots <- lapply(list(
-  list(var = "logrank_power" , title = "Log-rank test"),
-  list(var = "maxcombo_power", title = "Maxcombo test"),
-  list(var = "rmst_power", title = "RMST test")
-),
+cc_plots <- lapply(args,
 function (description) {
   return(
-    cc_results %>% filter(n == 100) %>% ggplot(aes(
-      x = shape, y = !!sym(description[["var"]])
-    )) +
-      geom_point(aes(colour = p_cens), size = 3) +
+    cc_results %>% filter(n == description[["n"]] &
+                            t_duration == description[["t_duration"]]) %>%
+      ggplot(aes(
+        x = shape, y = !!sym(description[["var"]])
+      )) +
+      geom_point(aes(colour = p_cens), size = 2, show.legend = FALSE) +
       geom_smooth(
         aes(group = p_cens, colour = p_cens),
         method = "lm",
-        linewidth = 0.5
+        linewidth = 0.5,
+        show.legend = FALSE
       ) +
       geom_hline(yintercept = 0.8) +
       xlab('Shape') + ylab('Test Power') + scale_y_continuous(limits = c(0, 1.1)) +
-      ggtitle(label = description[["title"]]) +
-      guides(color = guide_legend(title = "Censoring probability"))
+      ggtitle(
+        label = paste0(description[["title"]], " N = ", description[["n"]],
+                                             " Trial Duration = ", description[["t_duration"]])
+      ) + guides(color = guide_legend(title = "Censoring probability")) +
+      theme(plot.title = element_text(size=10))
   )
 })
 
-le_plots <- lapply(list(
-  list(var = "logrank_power" , title = "Log-rank test"),
-  list(var = "maxcombo_power", title = "Maxcombo test"),
-  list(var = "rmst_power", title = "RMST test")
-),
+le_plots <- lapply(args,
 function (description) {
   return(
-    le_results %>% filter(n == 100) %>% ggplot(aes(
+    le_results %>% filter(n == description[["n"]] &
+                            t_duration == description[["t_duration"]]) %>% ggplot(aes(
       x = effect_time, y = !!sym(description[["var"]])
     )) +
-      geom_point(aes(colour = p_cens), size = 3) +
+      geom_point(aes(colour = p_cens), size = 3, show.legend = FALSE) +
       geom_smooth(
         aes(group = p_cens, colour = p_cens),
         method = "lm",
-        linewidth = 0.5
+        linewidth = 0.5,
+        show.legend = FALSE
       ) +
       geom_hline(yintercept = 0.8) +
-      xlab('Effect Time') + ylab('Test Power') + scale_y_continuous(limits = c(0.4, 1.1)) +
-      ggtitle(label = description[["title"]]) +
-      guides(color = guide_legend(title = "Censoring probability"))
+      xlab('Effect Time') + ylab('Test Power') + scale_y_continuous(limits = c(0.0, 1.1)) +
+      ggtitle(
+        label = paste0(description[["title"]], " N = ", description[["n"]],
+                       " Trial Duration = ", description[["t_duration"]])
+      ) +
+      guides(color = guide_legend(title = "Censoring probability")) +
+      theme(plot.title = element_text(size=10))
   )
 })
 
-ee_plots <- lapply(list(
-  list(var = "logrank_power" , title = "Log-rank test"),
-  list(var = "maxcombo_power", title = "Maxcombo test"),
-  list(var = "rmst_power", title = "RMST test")
-),
+ee_plots <- lapply(args,
 function (description) {
   return(
-    ee_results %>% filter(n == 100) %>% ggplot(aes(
+    ee_results %>% filter(n == description[["n"]] &
+                            t_duration == description[["t_duration"]]) %>% ggplot(aes(
       x = effect_time, y = !!sym(description[["var"]])
     )) +
       geom_point(aes(colour = p_cens), size = 3) +
@@ -67,16 +93,19 @@ function (description) {
       ) +
       geom_hline(yintercept = 0.8) +
       xlab('Effect Time') + ylab('Test Power') + scale_y_continuous(limits = c(0, 1.1)) +
-      ggtitle(label = description[["title"]]) +
-      guides(color = guide_legend(title = "Censoring probability"))
+      ggtitle(
+        label = paste0(description[["title"]], " N = ", description[["n"]],
+                       " Trial Duration = ", description[["t_duration"]])
+      ) +
+      guides(color = guide_legend(title = "Censoring probability")) +
+      theme(plot.title = element_text(size=10))
   )
 })
 
 # 2D plots of survival functions
 base <-
   ggplot() +
-  xlim(0, 5)
-
+  xlim(0, 2)
 
 cc_survival <- base + lapply(shape_list, function(shape) {
   fun_name <- paste0("fun.", shape)
@@ -189,55 +218,60 @@ le_hazard <- base + lapply(effect_time_list_le, function(effect_time) {
   guides(color = guide_legend(title = expression(
     paste("t" ["effect"]))))
 
-grid.arrange(
-  cc_plots[[1]],
-  cc_plots[[2]],
-  cc_plots[[3]],
-  cc_survival,
-  cc_hazard,
-  ncol = 3,
-  top = textGrob("Crossing Curves",
-                 gp = gpar(fontsize = 17)),
-  bottom = textGrob(
-    sprintf(
-      "Control group (red): ~exp(1)\n Treatment groups(green): Weibull(%.1f, shape)",
-      scale_2_cc
-    ),
-    gp = gpar(fontsize = 10)
+for (start in 3 * seq(0, (length(n_list) * length(t_duration_list) - 1))) {
+  grid.arrange(
+    cc_plots[[start+1]],
+    cc_plots[[start+2]],
+    cc_plots[[start+3]],
+    cc_survival,
+    cc_hazard,
+    ncol = 3,
+    top = textGrob("Crossing Curves",
+                   gp = gpar(fontsize = 10)),
+    bottom = textGrob(
+      sprintf(
+        "Control group (red): ~exp(1)\n Treatment groups(green): Weibull(%.1f, shape)",
+        scale_2_cc
+      ),
+      gp = gpar(fontsize = 10)
+    )
   )
-)
+}
 
-grid.arrange(
-  le_plots[[1]],
-  le_plots[[2]],
-  le_plots[[3]],
-  le_survival,
-  le_hazard,
-  ncol = 3,
-  top = textGrob("Late Effect",
-                 gp = gpar(fontsize = 17)),
-  bottom = textGrob(expression(
-    paste(
-      "Control group (red): ~exp(1)\nTreatment groups(green): ~exp(1) before t" ["effect"],
-      ", ~exp(0.5) thereafter"
-    )
-  ),
-  gp = gpar(fontsize = 10))
-)
+for (start in 3 * seq(0, (length(n_list) * length(t_duration_list) - 1))) {
+  grid.arrange(
+    le_plots[[start+1]],
+    le_plots[[start+2]],
+    le_plots[[start+3]],
+    le_survival,
+    le_hazard,
+    ncol = 3,
+    top = textGrob("Late Effect",
+                   gp = gpar(fontsize = 17)),
+    bottom = textGrob(expression(
+      paste(
+        "Control group (red): ~exp(1)\nTreatment groups(green): ~exp(1) before t" ["effect"],
+        ", ~exp(0.5) thereafter"
+      )
+    ),
+    gp = gpar(fontsize = 10))
+  )
+}
 
-
-grid.arrange(
-  ee_plots[[1]],
-  ee_plots[[2]],
-  ee_plots[[3]],
-  ncol = 3,
-  top = textGrob("Early Effect",
-                 gp = gpar(fontsize = 17)),
-  bottom = textGrob(expression(
-    paste(
-      "Control group (red): ~exp(1)\nTreatment groups(green): ~exp(1) before t" ["effect"],
-      ", ~exp(0.5) thereafter"
-    )
-  ),
-  gp = gpar(fontsize = 10))
-)
+for (start in 3 * seq(0, (length(n_list) * length(t_duration_list) - 1))) {
+  grid.arrange(
+    ee_plots[[start+1]],
+    ee_plots[[start+2]],
+    ee_plots[[start+3]],
+    ncol = 3,
+    top = textGrob("Early Effect",
+                   gp = gpar(fontsize = 17)),
+    bottom = textGrob(expression(
+      paste(
+        "Control group (red): ~exp(1)\nTreatment groups(green): ~exp(1) before t" ["effect"],
+        ", ~exp(0.5) thereafter"
+      )
+    ),
+    gp = gpar(fontsize = 10))
+  )
+}
