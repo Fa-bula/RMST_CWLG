6cbca5601dd657a04b8cbfd31c5eea2c121ddf0b
fa_bula <fa_bula@mail.ru>
2023-12-23 19:09:03 +0300

    Added KM plot and HR table created based on reconstructed data

--- reconstruct_data.R
+++ reconstruct_data.R
@@ -1,6 +1,8 @@
 library(KMtoIPD)
 library(dplyr)
 library(survival)
+library(gtsummary)
+library(ggsurvfit)
 
 library(nph)
 library(survRM2)
@@ -25,6 +27,26 @@ IPD_list <- lapply(cohort_info,
 
 IPD <-
   rbind(IPD_list[[1]], IPD_list[[2]]) %>%
-  mutate(arm = as.numeric(factor(IPD$cohort)) - 1)
+  mutate(arm = -as.numeric(factor(cohort)) + 2)
 
 run_tests(df = IPD)
+
+coxph(Surv(aval, event) ~ arm, data = IPD) %>% tbl_regression(exp = TRUE)
+
+survfit2(Surv(aval, event) ~ cohort, data = IPD) %>% 
+  ggsurvfit() +
+  labs(
+    x = "Time since randomisation (months)",
+    y = "Overall survival"
+  ) +
+  add_censor_mark(size = 3, alpha = 0.4) +
+  scale_x_continuous(labels = seq(0,40,5),
+                     breaks = seq(0,40,5),
+                     limits = c(0,40),
+                     expand = c(0,1)) +
+  scale_y_continuous(labels = scales::percent,
+                     breaks = seq(0,1,0.1),
+                     limits = c(0,1),
+                     ) +
+  add_risktable(risktable_stats = "{n.risk} ({cum.censor})",
+                times = seq(0,40,5))
